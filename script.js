3315589b4ea1b4bfee482d502af893f328e517ab
michele camera <michy.cam95@gmail.com>
2022-03-09 15:25:00 +0100

    created dice game

--- script.js
+++ script.js
@@ -17,23 +17,31 @@ if (prompt.value === email[0]) {
 
 // Dadi
 
+const diceone = document.querySelector(".dice-1");
+const dicetwo = document.querySelector(".dice-2");
+const diceresult = document.querySelector(".dice-result");
+
+
 document.querySelector('.btn-roll').addEventListener('click', function () {
 
     // 1. Random number
     var diceman = Math.floor(Math.random() * 6) + 1;
     var dicecomputer = Math.floor(Math.random() * 6) + 1;
+    diceone.innerHTML += diceman;
+    dicetwo.innerHTML += dicecomputer;
 
     if (diceman > dicecomputer) {
         console.log("Vince Giocatore");
+        document.querySelector("h1").innerHTML = ("USER WINS!");
+
     } else if (diceman < dicecomputer) {
         console.log("Vince Computer");
+        document.querySelector("h1").innerHTML = ("PC WINS!");
     }
 
     // 2. display results 
 
-}
-
-);
+});
 
 
 
