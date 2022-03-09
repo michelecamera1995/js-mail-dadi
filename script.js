c606d2fa4796b57a78419c99953785853ed47a27
michele camera <michy.cam95@gmail.com>
2022-03-09 14:36:26 +0100

    creation script

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 console.log("JS-OK")
+
+// Email check
+
+/*
+let email = ["michy@gmail.com"]
+
+email.push(prompt("inserisci indirizzo email"))
+
+console.log(email);
+
+
+if (prompt.value === email[0]) {
+    alert = "email già inserita"
+}
+*/
+
+// Dadi
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+
+    // 1. Random number
+    var diceman = Math.floor(Math.random() * 6) + 1;
+    var dicecomputer = Math.floor(Math.random() * 6) + 1;
+
+    if (diceman > dicecomputer) {
+        console.log("Vince Giocatore");
+    } else if (diceman < dicecomputer) {
+        console.log("Vince Computer");
+    }
+
+    // 2. display results 
+
+}
+
+);
+
+
+
+
+
